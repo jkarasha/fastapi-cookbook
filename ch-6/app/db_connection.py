ac68b429efc264e5d6d68393ea820642999d44d1
Joe Karasha <jkarasha@gmail.com>
2025-02-10 19:15:11 -0800

    Add some comments/documentation.

--- ch-6/app/db_connection.py
+++ ch-6/app/db_connection.py
@@ -4,17 +4,23 @@ from sqlalchemy.ext.asyncio import (
 )
 from sqlalchemy.orm import sessionmaker
 
+# DB Name, should pull from a configuration file
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./database.db"
 
+# Using DB URL, connect to a 
 def get_engine():
     return create_async_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        echo=True,
     )
 
+# used to encapsulate a session, each request will get its own session
 AsyncSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=get_engine(), class_=AsyncSession
 )
 
 async def get_db_session():
+    #using async with creates a scoped call. Session closed and connection closed when the scope ends
     async with AsyncSessionLocal() as session:
         yield session
